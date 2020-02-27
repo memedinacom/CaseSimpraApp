bd532b0ab50d9810e22bb9cfe180cd5ddf10870a
Mehmet Zeytin <d-mzeytin@ziraatteknoloji.com>
2020-02-27 20:13:00 +0300

    Initial Commit

---
+++ SimpraApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpraApp
+//
+//  Created by Mehmet Zeytin on 27.02.2020.
+//  Copyright © 2020 Mehmet Zeytin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
