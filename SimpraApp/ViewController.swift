f53d83044579911c1947e102df097e89f573bb6e
Mehmet Zeytin <mehmet@hotelemek.com>
2020-03-01 00:29:48 +0300

    - Controller realm function kaldırıldı.

--- SimpraApp/ViewController.swift
+++ SimpraApp/ViewController.swift
@@ -18,30 +18,7 @@ class ViewController: UIViewController {
         
         SimpraApi.employess.fetchs(type: EmployeesResponseModel.self) { (response, error) in
             
-            do {
-              
-                let realm = try Realm()
-                           print(realm.configuration.fileURL?.absoluteString ?? "")
-                           
-                           try realm.write {
-                            
-                            for realmUserModel  in response!.employees!{
-                                let storedEmploye = Specimen()
-//                                storedEmploye.identifier = Int(realmUserModel.id!)!
-//                                storedEmploye.realName = realmUserModel.lastName!
-//                                storedEmploye.pin = realmUserModel.pin!
-//                                storedEmploye.name = realmUserModel.firstName!
-                                storedEmploye.latitude = 3244
-                                realm.add(storedEmploye ,update: .all)
-                            }
-                            
-                               
 
-                    }
-                
-                
-            } catch {
-            }
             
         }
     
