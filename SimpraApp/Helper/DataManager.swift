82250f0fe4171da1000d72a1040a333a11c20147
Mehmet Zeytin <mehmet@hotelemek.com>
2020-03-01 13:06:07 +0300

    - Data manager

---
+++ SimpraApp/Helper/DataManager.swift
@@ -0,0 +1,32 @@
+//
+//  DataManager.swift
+//  SimpraApp
+//
+//  Created by Mehmet Zeytin on 1.03.2020.
+//  Copyright © 2020 Mehmet Zeytin. All rights reserved.
+//
+
+import Foundation
+open class  DataManager {
+    var sectionModel:SectionResponse?
+    var actionModel:ActionResponseModel?
+    var employeeModel:EmployeesResponseModel?
+    var productModel:ProductResponseModel?
+    var tablesModel:TablesResponseModel?
+    var discounModel:DiscountResponseModel?
+    var groups:GroupsResponseModel?
+    
+    struct Singleton {
+           static let sharedInstance = DataManager()
+       }
+    class var sharedInstance: DataManager {
+        return Singleton.sharedInstance
+    }
+    
+    // MARK: - HotDrink
+    //**TWVudUl0ZW1Hcm91cC01MWNkZGQ5Zi01OGQ5LTRmZWMtYTRiNy03YTRlYmVjZjc3MGY**/
+    
+  
+    
+   
+}
