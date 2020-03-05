1ffe913563d186103ddec548426aa229f325cdeb
Mehmet Zeytin <mehmet@hotelemek.com>
2020-03-05 16:00:26 +0300

    - Realm DB üzerinden Örn. Login Auth.  geliştirmesi yapıldı.

--- SimpraApp/Controller/LoginViewController.swift
+++ SimpraApp/Controller/LoginViewController.swift
@@ -7,27 +7,46 @@
 //
 
 import Foundation
+import UIKit
+
 class LoginViewController: BaseViewController {
-// MARK: - Outlets
+    // MARK: - Outlets
     @IBOutlet weak var txtPassword: RoundedTextField!
     @IBOutlet weak var txtUsername: RoundedTextField!
+    
+    // MARK: - Lifecyle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       
+        self.prepareUI()
         
     }
-       // MARK: - Lifecyle
-       // MARK: - Properties
-       // MARK: - Functions
-       // MARK: - Actions
     
+    // MARK: - Functions
+    
+    // MARK: - Prepare UI
+    func prepareUI(){
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Login
     @IBAction func touchLogin(_ sender: Any) {
-        self.goHome()
+        
+        do {
+            guard let db = try? SimpraDB() else {return}
+            db.loginWithUser(firstName: "Hasan", lastname: "Soysal", password: txtPassword.text!, closure: { (auth) in
+                if auth{
+                    print("User Authhanticated")
+                    self.goHome()
+                }else{
+                    SimpraNotify.presentTopFloat(description: "Kullanıcı adı veya pin yanlış - pin 998899", image: nil, color: UIColor.redToast)
+                }
+            })
+        }
+        
     }
     // MARK: - Protocol
     
-
-
 }
 
