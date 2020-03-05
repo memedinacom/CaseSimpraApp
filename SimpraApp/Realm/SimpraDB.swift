cc4eef6b86a9aecd9e1f7b1334904f46c212da17
Mehmet Zeytin <mehmet@hotelemek.com>
2020-03-05 15:56:23 +0300

    - Realm Database Manager için ara katman yazıldı

--- SimpraApp/Realm/SimpraDB.swift
+++ SimpraApp/Realm/SimpraDB.swift
@@ -9,12 +9,50 @@
 import Foundation
 import RealmSwift
 
+public typealias SimpraModelCallback = ((_ status: Bool) -> Void)
+
 class SimpraDB {
     private let realm: Realm
 
     init() throws {
         self.realm = try Realm()
+        print(self.realm.configuration.fileURL)
+        
     }
+    
+    
+    //Employee save data
+    func saveUserData(_ data: EmployeeModel){
+        do {
+            try realm.write {
+                for item in data.employees {
+                    realm.add(item)
+                }
+            }
+        } catch let error as NSError {
+            
+        }
+    }
+    
+    func isValidDataEmployee()->Bool{
+        do {
+            guard let db = try? SimpraDB() else {return false}
+            let employeList = db.realm.objects(employee_item.self)
+            if (employeList.count > 0) { return true} else { return false}
+        }
+       
+    }
+    
+    // MARK: - Login employee from realm
+    func loginWithUser(firstName:String , lastname lastName:String , password pin:String , closure: @escaping SimpraModelCallback){
+           let users = realm.objects(employee_item.self).filter("first_name = '\(firstName)' && last_name = '\(lastName)' && pin = '\(pin)' ")
+           if users.count > 0 {
+               closure(true)
+           }else{
+               closure(false)
+           }
+           
+       }
 
 //        func saveSection(_ section: Foo) throws {
 //            try realm.write {
